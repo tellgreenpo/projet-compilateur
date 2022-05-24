937f1e72ecbb0b4b60d18ed17f615cbf3f9ca71a
tellgreenpo <mateo1000a@gmail.com>
2022-05-24 16:40:22 +0200

    Finished jump stack and transition from file to assembly table related functions

--- source/jump_stack.c
+++ source/jump_stack.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-Node * ifStack = NULL;
-Node * whileStack = NULL;
+Node *jumpStack = NULL;
+Node *whileStack = NULL;
 
-void add_while(int lineNumber){
+void add_while(int lineNumber)
+{
     // create a link
     Node *link = (Node *)malloc(sizeof(Node));
 
-    link -> lineNumber = lineNumber;
+    link->lineNumber = lineNumber;
 
     // point it to old first node
     link->next = whileStack;
@@ -18,39 +19,44 @@ void add_while(int lineNumber){
     whileStack = link;
 }
 
-void add_JMF(int lineNumber){
+void add_JMF(int lineNumber)
+{
     // create a link
     Node *link = (Node *)malloc(sizeof(Node));
 
-    link -> lineNumber = lineNumber;
+    link->lineNumber = lineNumber;
 
     // point it to old first node
-    link->next = ifStack;
+    link->next = jumpStack;
 
     // point first to new first node
-    ifStack = link;
+    jumpStack = link;
 }
 
-int pop_while(){
-    if (whileStack = NULL){
+int pop_while()
+{
+    if (whileStack = NULL)
+    {
         printf("Poping nothing in while stack bruv!\n");
         return -1;
     };
     int line = whileStack->lineNumber;
-    Node * aux = whileStack;
+    Node *aux = whileStack;
     whileStack = whileStack->next;
     free(aux);
     return line;
 }
 
-int pop_JMF(){
-    if (ifStack = NULL){
+int pop_JMF()
+{
+    if (jumpStack = NULL)
+    {
         printf("Poping nothing in jump stack bruv!\n");
         return -1;
     };
-    int line = ifStack->lineNumber;
-    Node * aux = ifStack;
-    ifStack = ifStack->next;
+    int line = jumpStack->lineNumber;
+    Node *aux = jumpStack;
+    jumpStack = jumpStack->next;
     free(aux);
     return line;
 }
