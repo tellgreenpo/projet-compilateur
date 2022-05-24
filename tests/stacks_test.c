57cd536ee6c1199fb84e8e05ab4efe7e391d5982
tellgreenpo <mateo1000a@gmail.com>
2022-05-24 16:59:13 +0200

    Added test file

---
+++ tests/stacks_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "../include/jump_stack.h"
+#include "../include/instructions.h"
+
+extern Cell *head;
+extern Node *jumpStack;
+extern Node *whileStack;
+
+// Testing add
+int main(int argc, char *argv[]){
+    add(1,1,1,1);
+    substract(2,2,2,2);
+    add_ASM_file();
+
+    add_while(99);
+    add_JMF(200);
+    add_while(88);
+    printf("Expected: 88\n");
+    printf("Result: %i",pop_while());
+    printf("Expected: 99\n");
+    printf("Result: %i",pop_while());
+    printf("Expected: 200\n");
+    printf("Result: %i",pop_JMF());
+}
